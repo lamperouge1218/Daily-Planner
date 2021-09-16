d6a116e032efbab779ec78ec75bedb0580ae4162
lamperouge <philbohn1791@gmail.com>
2021-09-16 13:41:25 -0400

    got JS page linked to HTML doc, set up function to have constantly updating date/time in jumbotron

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
+// good text for the header to be the date/time
+
+
+
+// setInterval(() => {
+    
+// }, 1000);
+// Will need to set the interval and have the timer auto update every second
+
+function currentTime() {
+    setInterval(() => {
+        var time = moment().format("MMM Do, YYYY, HH:mm:ss a"); 
+        $("#currentDay").text(time);
+    }, 1000);
+};
+
+currentTime();
+
+
+
+
