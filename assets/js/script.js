3db3b28884bcb8e004b4fbf75c5fef42dc77d65f
lamperouge <philbohn1791@gmail.com>
2021-09-17 18:39:19 -0400

    finished it lol

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-// moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
-// good text for the header to be the date/time
 var timeBlock = $(".time-day");
 var appForm9 = $(".appointment9");
 var appForm10 = $(".appointment10");
@@ -39,91 +37,74 @@ function currentTime() {
 
 currentTime();
 
-// GIVEN I am using a daily planner to create a schedule
-
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-    // This is done. 
-
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-    // Done
-
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-    // See stuff in slack. Possibly tying current time to some hour, possibly for each block
-    // if statment such that 
-
 var format = "H";
 var currentTime = moment().format(format);
-// var getCurrentTimeint = parseInt(currentTime);
-// console.log(getCurrentTimeint);
 console.log(currentTime);
 
 $("input").each (function () {
-    var hour9 = parseInt($(this).attr("id"));
-    console.log(hour9);
-    if (hour9 < currentTime){
+    var hour = parseInt($(this).attr("id"));
+    console.log(hour);
+    if (hour < currentTime){
         $(this).addClass("past")
-    } else if (hour9 > currentTime) {
+    } else if (hour > currentTime) {
         $(this).addClass("future")
     } else {
         $(this).addClass("present")
     }
 });
 
-function save9(event) {
+function save9() {
     var description9 = $(appForm9).val();
-    $(event.target);
     console.log(description9);
+    localStorage.setItem("9 appt", JSON.stringify(description9));
 };
 
-function save10(event) {
+function save10() {
     var description10 = $(appForm10).val();
-    $(event.target);
     console.log(description10);
+    localStorage.setItem("10 appt", JSON.stringify(description10));
 };
 
-function save11(event) {
+function save11() {
     var description11 = $(appForm11).val();
-    $(event.target);
     console.log(description11);
+    localStorage.setItem("11 appt", JSON.stringify(description11));
 };
 
-function save12(event) {
+function save12() {
     var description12 = $(appForm12).val();
-    $(event.target);
     console.log(description12);
+    localStorage.setItem("12 appt", JSON.stringify(description12));
 };
 
-function save13(event) {
+function save13() {
     var description13 = $(appForm13).val();
-    $(event.target);
     console.log(description13);
+    localStorage.setItem("13 appt", JSON.stringify(description13));
 };
 
-function save14(event) {
+function save14() {
     var description14 = $(appForm14).val();
-    $(event.target);
     console.log(description14);
+    localStorage.setItem("14 appt", JSON.stringify(description14));
 };
 
-function save15(event) {
+function save15() {
     var description15 = $(appForm15).val();
-    $(event.target);
     console.log(description15);
+    localStorage.setItem("15 appt", JSON.stringify(description15));
 };
 
-function save16(event) {
+function save16() {
     var description16 = $(appForm16).val();
-    $(event.target);
     console.log(description16);
+    localStorage.setItem("16 appt", JSON.stringify(description16));
 };
 
-function save17(event) {
+function save17() {
     var description17 = $(appForm17).val();
-    $(event.target);
     console.log(description17);
+    localStorage.setItem("17 appt", JSON.stringify(description17));
 };
 
 saveBtn9.on("click", save9);
@@ -136,7 +117,82 @@ saveBtn15.on("click", save15);
 saveBtn16.on("click", save16);
 saveBtn17.on("click", save17);
 
+var appData9 = JSON.parse(localStorage.getItem("9 appt"));
+var appData10 = JSON.parse(localStorage.getItem("10 appt"));
+var appData11 = JSON.parse(localStorage.getItem("11 appt"));
+var appData12 = JSON.parse(localStorage.getItem("12 appt"));
+var appData13 = JSON.parse(localStorage.getItem("13 appt"));
+var appData14 = JSON.parse(localStorage.getItem("14 appt"));
+var appData15 = JSON.parse(localStorage.getItem("15 appt"));
+var appData16 = JSON.parse(localStorage.getItem("16 appt"));
+var appData17 = JSON.parse(localStorage.getItem("17 appt"));
 
+function addData9() {
+    if (appData9 !== null) {
+        $(appForm9).val(appData9);
+    }
+};
+
+function addData10() {
+    if (appData10 !== null) {
+        $(appForm10).val(appData10);
+    }
+};
+
+function addData11() {
+    if (appData11 !== null) {
+        $(appForm11).val(appData11);
+    }
+};
+
+function addData12() {
+    if (appData12 !== null) {
+        $(appForm12).val(appData12);
+    }
+};
+
+function addData13() {
+    if (appData13 !== null) {
+        $(appForm13).val(appData13);
+    }
+};
+
+function addData14() {
+    if (appData14 !== null) {
+        $(appForm14).val(appData14);
+    }
+};
+
+function addData15() {
+    if (appData15 !== null) {
+        $(appForm15).val(appData15);
+    }
+};
+
+function addData16() {
+    if (appData16 !== null) {
+        $(appForm16).val(appData16);
+    }
+};
+
+function addData17() {
+    if (appData17 !== null) {
+        $(appForm17).val(appData17);
+    }
+};
+
+
+addData9();
+addData10();
+addData11();
+addData12();
+addData13();
+addData14();
+addData15();
+addData16();
+addData17();
+
+// make a variable that is the .getItem equal to the stored value that we save on saveBtn click 
 
 
 // if (Element.dataset.hour < getCurrentTimeint) {
