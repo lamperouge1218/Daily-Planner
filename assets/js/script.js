7a9c701469e6e32b8f6fcb5a733c199ccd228ddf
lamperouge <philbohn1791@gmail.com>
2021-09-16 22:36:38 -0400

    styling for page completed. moving on to the JQuery logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,6 @@
 var time = moment().format("dddd, MMM Do, YYYY, HH:mm:ss a"); 
 $("#currentDay").text(time);
 
-// setInterval(() => {
-    
-// }, 1000);
-// Will need to set the interval and have the timer auto update every second
-
 function currentTime() {
     setInterval(() => {
         var time = moment().format("dddd, MMM Do, YYYY, HH:mm:ss a"); 
@@ -18,6 +13,28 @@ function currentTime() {
 
 currentTime();
 
+// GIVEN I am using a daily planner to create a schedule
+
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+    // This is done. 
+
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours
+
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+
+// WHEN I click into a timeblock
+// THEN I can enter an event
+
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+    // Button listener event throwing stuff into local storage. 
+    // Similar to previous assignment? Sending it to an array for string concat?
+
+// WHEN I refresh the page
+// THEN the saved events persist
 
 
 
